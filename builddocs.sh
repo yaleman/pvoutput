0d69417b54f5ded100ddf1bb9ae0f8aa40b150f6
yaleman <james@terminaloutcomes.com>
2021-12-27 15:24:31 +1000

    removing old scripts

--- builddocs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# shellcheck disable=SC1091
-source venv/bin/activate
-
-cd docs || exit
-make clean html
-cd ..
--- buildpackages.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-rm dist/*
-python setup.py sdist #bdist_wheel
-
-#./builddocs.sh
