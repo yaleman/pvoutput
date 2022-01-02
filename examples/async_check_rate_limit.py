20fb308bb8c28a292957350beb038d86c2dd1695
Joram Agten <joram.agten@vito.be>
2022-01-02 05:00:47 +0100

    examples

---
+++ examples/async_check_rate_limit.py
@@ -0,0 +1,29 @@
+import datetime
+
+import aiofiles
+import aiohttp
+import asyncio
+import json
+from pvoutput.asyncio import PVOutput
+
+
+async def get_apikey_systemid():
+    async with aiofiles.open("pvoutput.json", mode="r", encoding="utf8") as f:
+        contents = await f.read()
+    config_data = json.loads(contents)
+    return config_data["apikey"], config_data["systemid"]
+
+
+async def main():
+    apikey, systemid = await get_apikey_systemid()
+
+    async with aiohttp.ClientSession() as session:
+        pvo = PVOutput(apikey=apikey, systemid=systemid, session=session)
+        result = await pvo.check_rate_limit()
+    print(json.dumps(result, indent=2))
+
+    reset_datetime = datetime.datetime.fromtimestamp(int(result['X-Rate-Limit-Reset']), datetime.timezone.utc)
+    print(f"{reset_datetime.isoformat()=}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ examples/sync_check_rate_limit.py
@@ -0,0 +1,25 @@
+import datetime
+
+from pvoutput import PVOutput
+import json
+
+
+def get_apikey_systemid():
+    with open("pvoutput.json", "r", encoding="utf8") as config_file:
+        config_data = json.load(config_file)
+    return config_data["apikey"], config_data["systemid"]
+
+
+def main():
+    apikey, systemid = get_apikey_systemid()
+
+    pvo = PVOutput(apikey=apikey, systemid=systemid)
+    result = pvo.check_rate_limit()
+    print(json.dumps(result, indent=2))
+
+    reset_datetime = datetime.datetime.fromtimestamp(int(result['X-Rate-Limit-Reset']), datetime.timezone.utc)
+    print(f"{reset_datetime.isoformat()=}")
+
+
+if __name__ == "__main__":
+    main()
